2b49cba5c1e4f530a9700410f08b527b2e2a55d7
phm87 <31578435+phm87@users.noreply.github.com>
2018-09-15 23:56:04 +0200

    Create bitfinex.php

---
+++ web/yaamp/core/exchange/bitfinex.php
@@ -0,0 +1,21 @@
+<?php
+
+// https://api.bitfinex.com/v1/symbols
+// https://api.bitfinex.com/v1/pubticker/btcusd
+
+function bitfinex_api_query($method, $params='', $returnType='object')
+{
+	$uri = "https://api.bitfinex.com/v1/{$method}";
+	if (!empty($params)) $uri .= "/{$params}";
+ 	$ch = curl_init($uri);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ 	$execResult = strip_tags(curl_exec($ch));
+ 	if ($returnType == 'object')
+		$ret = json_decode($execResult);
+	else
+		$ret = json_decode($execResult,true);
+ 	return $ret;
+}
