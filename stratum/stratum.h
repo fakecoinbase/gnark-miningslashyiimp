fb8f785fd1ddb6e25d99c861dfe023211ffe7d93
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2019-01-05 04:07:48 -0500

    Update stratum.h

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -171,6 +171,7 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/lyra2re.h"
 #include "algos/lyra2v2.h"
 #include "algos/lyra2z.h"
+#include "algos/lyra2vc0ban.h"
 #include "algos/blake.h"
 #include "algos/blakecoin.h"
 #include "algos/blake2b.h"
