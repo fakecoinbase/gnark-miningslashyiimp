0acd0a65f9cb505c13229752427d0bccaeb4c870
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2019-02-03 22:44:38 -0500

    Update stratum.cpp

--- stratum/stratum.cpp
+++ stratum/stratum.cpp
@@ -186,6 +186,7 @@ YAAMP_ALGO g_algos[] =
 	{"rainforest", rainforest_hash, 0x100, 0, 0},
 	{"skein2", skein2_hash, 1, 0, 0},
 	{"yescrypt", yescrypt_hash, 0x10000, 0, 0},
+	{"yescryptR8", yescryptR8_hash, 0x10000, 0, 0},
 	{"yescryptR16", yescryptR16_hash, 0x10000, 0, 0 },
 	{"yescryptR32", yescryptR32_hash, 0x10000, 0, 0 },
 	{"yespower", yespower_hash, 0x10000, 0, 0 },
