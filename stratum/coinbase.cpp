c926b8bc8c91a03f9d07b20c670f56a92792c550
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2019-02-03 22:26:10 -0500

    Update coinbase.cpp

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -392,7 +392,7 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 		return;
 
 	}
-	else if ((strcmp(coind->symbol, "IFX") == 0)||(strcmp(coind->symbol, "GTM") == 0)||(strcmp(coind->symbol, "RITO") == 0)||(strcmp(coind->symbol, "AGM") == 0)||(strcmp(coind->symbol, "BMN") == 0))||(strcmp(coind->symbol, "CRDS") == 0))
+	else if ((strcmp(coind->symbol, "IFX") == 0)||(strcmp(coind->symbol, "GTM") == 0)||(strcmp(coind->symbol, "AGM") == 0)||(strcmp(coind->symbol, "BMN") == 0)||(strcmp(coind->symbol, "CRDS") == 0))
 	{
 		char payees[4];
 		int npayees = 1;
