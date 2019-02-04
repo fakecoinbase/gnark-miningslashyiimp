aded6ae049c506482916daf5ae8ec51028121f85
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2019-02-03 23:22:21 -0500

    Update stratum.cpp

--- stratum/stratum.cpp
+++ stratum/stratum.cpp
@@ -134,6 +134,7 @@ YAAMP_ALGO g_algos[] =
 
 	{"x16r", x16r_hash, 0x100, 0, 0},
 	{"x16s", x16s_hash, 0x100, 0, 0},
+	{"x16rt", x16rt_hash, 0x100, 0, 0},
 	{"timetravel", timetravel_hash, 0x100, 0, 0},
 	{"bitcore", timetravel10_hash, 0x100, 0, 0},
 	{"exosis", exosis_hash, 0x100, 0, 0},
