5405ce9f0b2f5248b1fe4dd3cc535aaa1c10230f
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2019-02-20 00:03:13 -0500

    Create binarium_hash_v1.h

---
+++ stratum/algos/binarium_hash_v1.h
@@ -0,0 +1,16 @@
+#ifndef Binarium_hash_v1_H
+#define Binarium_hash_v1_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void Binarium_hash_v1_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
