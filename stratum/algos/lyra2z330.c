f627e785253079a15e5c74f16dc13b16f40f3f68
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2020-03-26 08:03:53 -0400

    Update lyra2z330.c

--- stratum/algos/lyra2z330.c
+++ stratum/algos/lyra2z330.c
@@ -20,7 +20,7 @@ void lyra2z330_hash(void *state, const void *input, uint32_t height)
 {
 	uint32_t _ALIGN(256) hash[16];
 
-   LYRA2Z( lyra2z330_wholeMatrix, hash, 32, input, 80, input, 80, 2, 330, 256 );
+   LYRA2Z330( lyra2z330_wholeMatrix, hash, 32, input, 80, input, 80, 2, 330, 256 );
 
 	memcpy(state, hash, 32);
 }
@@ -34,9 +34,9 @@ int scanhash_lyra2z330( struct work *work, uint32_t max_nonce,
    uint32_t *ptarget = work->target;
    const uint32_t first_nonce = pdata[19];
    uint32_t nonce = first_nonce;
-   const int thr_id = mythr->id; 
 
-   if (opt_benchmark)
+
+
 	ptarget[7] = 0x0000ff;
 
    casti_m128i( edata, 0 ) = mm128_bswap_32( casti_m128i( pdata, 0 ) );
@@ -75,7 +75,7 @@ bool lyra2z330_thread_init()
    return lyra2z330_wholeMatrix;
 }
 
-bool register_lyra2z330_algo( algo_gate_t* gate )
+
 {
   gate->optimizations = SSE42_OPT | AVX2_OPT;
   gate->miner_thread_init = (void*)&lyra2z330_thread_init;
