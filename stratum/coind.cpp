631623a217d48ee15459a1ffeae6beec244685ca
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2020-04-01 19:34:37 -0400

    workcoin DNA fix

--- stratum/coind.cpp
+++ stratum/coind.cpp
@@ -115,7 +115,7 @@ bool coind_validate_address(YAAMP_COIND *coind)
 	sprintf(params, "[\"%s\"]", coind->wallet);
 
 	json_value *json;
-    bool getaddressinfo = ((strcmp(coind->symbol,"WAZ") == 0) || (strcmp(coind->symbol2, "WAZ") == 0) || (strcmp(coind->symbol,"DGB") == 0) || (strcmp(coind->symbol2, "DGB") == 0) || (strcmp(coind->symbol, "IBTC") == 0) || (strcmp(coind->symbol2, "IBTC") == 0) || (strcmp(coind->symbol, "WORK") == 0) || (strcmp(coind->symbol2, "WORK") == 0) || (strcmp(coind->symbol, "BTCVT") == 0) || (strcmp(coind->symbol2, "BTCVT") == 0) || (strcmp(coind->symbol, "AUR") == 0) || (strcmp(coind->symbol2, "AUR") == 0) || (strcmp(coind->symbol, "BFL") == 0) || (strcmp(coind->symbol2, "BFL") == 0) || (strcmp(coind->symbol, "PIGY") == 0) || (strcmp(coind->symbol2, "PIGY") == 0));	
+    bool getaddressinfo = ((strcmp(coind->symbol,"WAZ") == 0) || (strcmp(coind->symbol2, "WAZ") == 0) || (strcmp(coind->symbol,"DGB") == 0) || (strcmp(coind->symbol2, "DGB") == 0) || (strcmp(coind->symbol, "IBTC") == 0) || (strcmp(coind->symbol2, "IBTC") == 0) || (strcmp(coind->symbol, "DNA") == 0) || (strcmp(coind->symbol2, "DNA") == 0) || (strcmp(coind->symbol, "BTCVT") == 0) || (strcmp(coind->symbol2, "BTCVT") == 0) || (strcmp(coind->symbol, "AUR") == 0) || (strcmp(coind->symbol2, "AUR") == 0) || (strcmp(coind->symbol, "BFL") == 0) || (strcmp(coind->symbol2, "BFL") == 0) || (strcmp(coind->symbol, "PIGY") == 0) || (strcmp(coind->symbol2, "PIGY") == 0));	
 	if(getaddressinfo)
 		json = rpc_call(&coind->rpc, "getaddressinfo", params);
 	else
@@ -204,9 +204,9 @@ void coind_init(YAAMP_COIND *coind)
         json = rpc_call(&coind->rpc, "getnewaddress", params);
     }
 	
-	    bool is_work = ((strcmp(coind->symbol,"WORK") == 0) || (strcmp(coind->symbol2, "WORK") == 0));
+	    bool is_dna = ((strcmp(coind->symbol,"DNA") == 0) || (strcmp(coind->symbol2, "DNA") == 0));
 
-    if (is_work) {
+    if (is_dna) {
         if (json) json_value_free(json);
 
         json = rpc_call(&coind->rpc, "getnewaddress", params);
