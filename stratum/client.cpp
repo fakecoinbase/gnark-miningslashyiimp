1b44a28da1f2699aab2df259a791e40e026652d0
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2020-04-01 13:18:47 -0400

    bad mining address remove log message

--- stratum/client.cpp
+++ stratum/client.cpp
@@ -267,7 +267,7 @@ bool client_authorize(YAAMP_CLIENT *client, json_value *json_params)
 	// when auto exchange is disabled, only authorize good wallet address...
 	if (!g_autoexchange && !client_validate_user_address(client)) {
 
-		clientlog(client, "bad mining address %s", client->username);
+		// clientlog(client, "bad mining address %s", client->username);
 		client_send_result(client, "false");
 
 		CommonLock(&g_db_mutex);
